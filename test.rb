3cfcf4569a28dccf6e3bbc29a980a79981c3b90c
nguyenkhoa2407 <nguyenkhoa2407@gmail.com>
2022-06-23 17:03:27 +0700

    new ver

--- test.rb
+++ test.rb
@@ -6,17 +6,17 @@ def create
     redirect_to new_admin_booking_item_path and return
   end
 
-  success_count = images.select do |image|
+  saved_item_count = images.select do |image|
     BookingItem.create(
       booking_id: booking_id, 
       status: 'stored', 
       description: image.original_filename.split(".")[0], 
       image: image 
     ).persisted?
-  end
+  end.count
 
-  if success_count > 0
-    flash[:notice] = "Created #{success_count} items successfully !"
+  if saved_item_count > 0
+    flash[:notice] = "Created #{saved_item_count} items successfully !"
     redirect_to collection_url
   end
 end

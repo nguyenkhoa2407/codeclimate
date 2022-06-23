77ce9a8102df82215a4772c79389d1dc6f78b747
nguyenkhoa2407 <nguyenkhoa2407@gmail.com>
2022-06-23 16:44:16 +0700

    new ver

--- test.rb
+++ test.rb
@@ -1,26 +1,24 @@
 def create
   images = permitted_params[:booking_item][:images]
-  booking_id = permitted_params[:booking_item][:booking_id]
-
-  unless images.present && booking_id.present?
-    redirect_to new_admin_booking_item_path and return
+  if !images || permitted_params[:booking_item][:booking_id].empty?
+    redirect_to new_admin_booking_item_path
+    return
   end
 
-  count_items_created_successfullly = 0
+  @count_items_created_successfullly = 0
 
-  images.each do |image|
-    new_item = BookingItem.new({ 
-      booking_id: booking_id, 
-      status: 'stored', 
-      description: item_description(image), 
-      image: image 
-    })
-    count_items_created_successfullly += 1 if new_item.save
-  end
+  images.each { |img|
+    @custom_description = img.original_filename.split(".")[0]
 
-  if count_items_created_successfullly > 0
-    flash[:notice] = "Create #{count_items_created_successfullly} items successfully !"
+    @item = BookingItem.new({ booking_id: permitted_params[:booking_item][:booking_id], status: "stored", description: @custom_description, image: img })
+
+    if @item.save
+      @count_items_created_successfullly += 1
+    end
+  }
+
+  if @count_items_created_successfullly > 0
+    flash[:notice] = "Create #{@count_items_created_successfullly} items successfully !"
     redirect_to collection_url
   end
 end
-

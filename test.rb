f9e1cb1bd9627133dd7c96e2ffa6cc6a64464210
nguyenkhoa2407 <nguyenkhoa2407@gmail.com>
2022-06-23 16:34:25 +0700

    test commit

---
+++ test.rb
@@ -0,0 +1,26 @@
+def create
+  images = permitted_params[:booking_item][:images]
+  booking_id = permitted_params[:booking_item][:booking_id]
+
+  unless images.present && booking_id.present?
+    redirect_to new_admin_booking_item_path and return
+  end
+
+  count_items_created_successfullly = 0
+
+  images.each do |image|
+    new_item = BookingItem.new({ 
+      booking_id: booking_id, 
+      status: 'stored', 
+      description: item_description(image), 
+      image: image 
+    })
+    count_items_created_successfullly += 1 if new_item.save
+  end
+
+  if count_items_created_successfullly > 0
+    flash[:notice] = "Create #{count_items_created_successfullly} items successfully !"
+    redirect_to collection_url
+  end
+end
+
